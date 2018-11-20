7636bf4a08b2b94ab44f948bd54d2c746c9a4f41
guoxinl <lgx31@sina.cn>
2018-11-20 10:31:39 +0800

    docker zone time

--- src/main/java/pub/guoxin/docker/DockerDemoApplication.java
+++ src/main/java/pub/guoxin/docker/DockerDemoApplication.java
@@ -8,7 +8,6 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
  */
 @SpringBootApplication
 public class DockerDemoApplication {
-
     public static void main(String[] args) {
         SpringApplication.run(DockerDemoApplication.class, args);
     }
--- src/main/java/pub/guoxin/docker/DockerDemoController.java
+++ src/main/java/pub/guoxin/docker/DockerDemoController.java
@@ -3,6 +3,9 @@ package pub.guoxin.docker;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
+
 /**
  * Create by guoxin on 2018/11/16
  */
@@ -11,6 +14,6 @@ public class DockerDemoController {
 
     @GetMapping("/docker-demo")
     public String helloWord() {
-        return "hello word";
+        return LocalDateTime.now().format(DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm:ss"));
     }
 }
